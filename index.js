828d35c134671e1543dcdb88ac03a9efab29d2c1
AlixReb0rn <32742967+AlixReb0rn@users.noreply.github.com>
2018-04-21 10:29:37 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,146 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '.tos') {
+    msg.reply(`DoxBin is a bot that allows you to search peoples doxes in our database also you can get growtopia passwords like leakedsource. have fun while using our bot.`);
+  }
+});
+client.on('message', msg => {
+    if (msg.content === '.search AFDABZ') {
+        color: 3447003,
+      msg.reply('```👤 Username :\r\AFDABZ \r\IP Address :' + '\r\NULL ' + '\r\Country (Can be vpn):\r\Finland/Suomi \r\Growtopia username:\r\AFDABZ1,AFDABZ2,AFDABZ3,AFDABZ10\r\Growtopia Password : \r\NULL```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.semi') {
+        color: 3447003,
+      msg.reply('``` 🕵  Monthly :\r\ 1.89$ , 25 World Locks @Ælix#5549 PM/  \r\ Year :\r\ 8.99$ , 3 Diamond Locks @Ælix#5549 PM\r\ when you buy this you can get database tables of blacklisted people \r\ & Search Growtopia users database tables & passwords decrypted```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.search haxrin') {
+        color: 3447003,
+      msg.reply('```👤 Username :\r\Haxrin, DarkNoobz, iHek \r\IP Address :' + '\r\84.42.161.126 ' + '\r\HIS REAL NAME:\r\Unknown \r\Growtopia username:\r\Unknown\r\Growtopia Password : \r\Unknown \r\Face Reveal : \r\https://image.ibb.co/mOHHMn/haxrin.jpg```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.search nadohack') {
+        color: 3447003,
+      msg.reply('```👤 Username :\r\NadoHack , Titangel \r\His phone number: ' + '\r\+994 51 693 93 97 ' + '\r\HIS REAL NAME:\r\Isa Bozkurt \r\IP :\r\5.197.89.204\r\Growtopia username:\r\G0BLlN\r\Growtopia Password : \r\9cf95dacd226dcf43da376cdb6cbba7035218921```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.search swegamerhd') {
+        color: 3447003,
+        msg.reply('```👤 Username :\r\SweGamer,SweGaming,SweGamerHD,SweGamerHD GT \r\Phone number: ' + '\r\Blacklisted ' + '\r\HIS REAL NAME:\r\Blacklisted \r\IP :\r\77.107.38.219\r\Growtopia username:\r\SweGamerHD\r\Growtopia Password : \r\4cf95dacd526dcf43da376cdb6cbba7035218921```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.search shield') {
+        color: 3447003,
+        msg.reply('```👤 Username :\r\Shield , kakka \r\Phone number: ' + '\r\Blacklisted ' + '\r\HIS REAL NAME:\r\Blacklisted \r\ISP :\r\Elisa Oyj\r\Growtopia username:\r\Blacklisted\r\Growtopia Password : \r\6bffcdacd526dcf43da376cdb6cbba7035218921```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.search alix') {
+        color: 3447003,
+        msg.reply('```👤 Username :\r\Your mama fat\r\Phone number: ' + '\r\wanna get swatted? ' + '\r\HIS REAL NAME:\r\i dont tkink soo \r\ISP :\r\Auschwitz Cell\r\Growtopia username:\r\fuck you\r\Growtopia Password : \r\neee```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.search reyizhax') {
+        color: 3447003,
+        msg.reply('```👤 Username :\r\Your mama fat\r\Phone number: ' + '\r\wanna get swatted? ' + '\r\HIS REAL NAME:\r\i dont tkink soo \r\ISP :\r\Auschwitz Cell\r\Growtopia username:\r\fuck you\r\Growtopia Password : \r\neee```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.help') {
+        color: 3447003,
+        msg.reply('```Help Commands \r\ .dox (name , email , surname , ip) \r\ .fbi fbi documents```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.search FineHax') {
+        color: 3447003,
+      msg.reply('```👤 Username :\r\Strix Agario, RelungGamer, NabDouce, FineHax, TheHumanGt, RektyGt \r\Email:rashadgamer123@gmail.com' +  '\r\IP Address: 36.75.114.251 \r\```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.search finehax') {
+        color: 3447003,
+      msg.reply('```👤 Username :\r\Strix Agario, RelungGamer, NabDouce, FineHax, TheHumanGt, RektyGt \r\Email:rashadgamer123@gmail.com' +  '\r\IP Address: 36.75.114.251 \r\```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.search amateurz') {
+        color: 3447003,
+      msg.reply('```👤 Username :\r\Amateurz, Ama \r\Card: Visa Electron & Number: 4716458381455325' +  '\r\Dad: Petri Kartunnen \r\Mom: Hannele Kartunnen \r\Self : Joni Kartunnen \r\Address: Kellontie 17 \r\Birth : 11 / 9 / 2000 \r\```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.dbs') {
+        color: 3447003,
+      msg.reply('```Our Database Breaches \r\Growtopiagame.com\r\Minecraft.net\r\4510 Database Registers Currently!```')
+      
+   
+   
+    }
+  });
+  client.on('message', msg => {
+    if (msg.content === '.search ztztopia') {
+        color: 3447003,
+      msg.reply('```Blacklisted! you think you can really dox a person like my brother? nah i dont think so. :)```')
+      
+   
+   
+    }
+  });
+  client.on('message',msg =>{
+if (msg.content=='.rv_ama'){
+msg.reply('https://pbs.twimg.com/profile_images/659598322206949376/GL0_Sood.jpg')
+}
+  
+});
+client.login('NDM2OTMxNzg2MDgyMjIyMTAx.DbusYA.j7EontvOtSF5uHSv3BT0iSlBuQI');
+
