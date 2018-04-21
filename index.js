9d2fb68f57ff7165ffaae0afe0ffa180b14b1728
AlixReb0rn <32742967+AlixReb0rn@users.noreply.github.com>
2018-04-21 11:08:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ client.on('message', msg => {
     }
   });
 client.on('message', msg => {
-    if (msg.content === '.search @seth') {
+    if (msg.content === '.search seth') {
         color: 3447003,
-      msg.reply('``` 👤 Username :\r\ @Seth,Seth,Seth A Robinsons \r\ Home :' + '\r\ 6-17-5 Makuyama, Fukuyama Hiroshima 721-0913 JP ' + '\r\ Phone: +81.849482151 \r\ Email:rtsoftjapan@gmail.com\r\ Growtopia Password : \r\2B607D78393C4CF12B61DC2FE0FC84167```')
+      msg.reply('```There is just 1 person in our database \r\ 👤 Username :\r\ @Seth,Seth,Seth A Robinsons \r\ Home :' + '\r\ 6-17-5 Makuyama, Fukuyama Hiroshima 721-0913 JP ' + '\r\ Phone: +81.849482151 \r\ Email:rtsoftjapan@gmail.com\r\ Growtopia Password : \r\ 2B607D78393C4CF12B61DC2FE0FC84167```')
       
    
    
@@ -40,7 +40,7 @@ client.on('message', msg => {
   client.on('message', msg => {
     if (msg.content === '.search haxrin') {
         color: 3447003,
-      msg.reply('```👤 Username :\r\Haxrin, DarkNoobz, iHek \r\IP Address :' + '\r\84.42.161.126 ' + '\r\HIS REAL NAME:\r\Unknown \r\Growtopia username:\r\Unknown\r\Growtopia Password : \r\Unknown \r\Face Reveal : \r\https://image.ibb.co/mOHHMn/haxrin.jpg```')
+      msg.reply('```There is just 1 person in our database \r\ 👤 Username :\r\Haxrin, DarkNoobz, iHek \r\IP Address :' + '\r\84.42.161.126 ' + '\r\HIS REAL NAME:\r\Unknown \r\Growtopia username:\r\Unknown\r\Growtopia Password : \r\Unknown \r\Face Reveal : \r\https://image.ibb.co/mOHHMn/haxrin.jpg```')
       
    
    
@@ -49,7 +49,7 @@ client.on('message', msg => {
   client.on('message', msg => {
     if (msg.content === '.search nadohack') {
         color: 3447003,
-      msg.reply('```👤 Username :\r\NadoHack , Titangel \r\His phone number: ' + '\r\+994 51 693 93 97 ' + '\r\HIS REAL NAME:\r\Isa Bozkurt \r\IP :\r\5.197.89.204\r\Growtopia username:\r\G0BLlN\r\Growtopia Password : \r\9cf95dacd226dcf43da376cdb6cbba7035218921```')
+      msg.reply('```There is just 1 person in our database \r\ 👤 Username :\r\NadoHack , Titangel \r\His phone number: ' + '\r\+994 51 693 93 97 ' + '\r\HIS REAL NAME:\r\Isa Bozkurt \r\IP :\r\5.197.89.204\r\Growtopia username:\r\G0BLlN\r\Growtopia Password : \r\9cf95dacd226dcf43da376cdb6cbba7035218921```')
       
    
    
@@ -58,7 +58,7 @@ client.on('message', msg => {
   client.on('message', msg => {
     if (msg.content === '.search swegamerhd') {
         color: 3447003,
-        msg.reply('```👤 Username :\r\SweGamer,SweGaming,SweGamerHD,SweGamerHD GT \r\Phone number: ' + '\r\Blacklisted ' + '\r\HIS REAL NAME:\r\Blacklisted \r\IP :\r\77.107.38.219\r\Growtopia username:\r\SweGamerHD\r\Growtopia Password : \r\4cf95dacd526dcf43da376cdb6cbba7035218921```')
+        msg.reply('```There is just 1 person in our database \r\ 👤 Username :\r\SweGamer,SweGaming,SweGamerHD,SweGamerHD GT \r\Phone number: ' + '\r\Blacklisted ' + '\r\HIS REAL NAME:\r\Blacklisted \r\IP :\r\77.107.38.219\r\Growtopia username:\r\SweGamerHD\r\Growtopia Password : \r\4cf95dacd526dcf43da376cdb6cbba7035218921```')
       
    
    
@@ -67,7 +67,7 @@ client.on('message', msg => {
   client.on('message', msg => {
     if (msg.content === '.search shield') {
         color: 3447003,
-        msg.reply('```👤 Username :\r\Shield , kakka \r\Phone number: ' + '\r\Blacklisted ' + '\r\HIS REAL NAME:\r\Blacklisted \r\ISP :\r\Elisa Oyj\r\Growtopia username:\r\Blacklisted\r\Growtopia Password : \r\6bffcdacd526dcf43da376cdb6cbba7035218921```')
+        msg.reply('```There is just 1 person in our database \r\ 👤 Username :\r\Shield , kakka \r\Phone number: ' + '\r\Blacklisted ' + '\r\HIS REAL NAME:\r\Blacklisted \r\ISP :\r\Elisa Oyj\r\Growtopia username:\r\Blacklisted\r\Growtopia Password : \r\6bffcdacd526dcf43da376cdb6cbba7035218921```')
       
    
    
@@ -76,7 +76,7 @@ client.on('message', msg => {
   client.on('message', msg => {
     if (msg.content === '.search alix') {
         color: 3447003,
-        msg.reply('```👤 Username :\r\Your mama fat\r\Phone number: ' + '\r\wanna get swatted? ' + '\r\HIS REAL NAME:\r\i dont tkink soo \r\ISP :\r\Auschwitz Cell\r\Growtopia username:\r\fuck you\r\Growtopia Password : \r\neee```')
+        msg.reply('```There is just 1 person in our database \r\ 👤 Username :\r\Your mama fat\r\Phone number: ' + '\r\wanna get swatted? ' + '\r\HIS REAL NAME:\r\i dont tkink soo \r\ISP :\r\Auschwitz Cell\r\Growtopia username:\r\fuck you\r\Growtopia Password : \r\neee```')
       
    
    
@@ -85,7 +85,7 @@ client.on('message', msg => {
   client.on('message', msg => {
     if (msg.content === '.search reyizhax') {
         color: 3447003,
-        msg.reply('```👤 Username :\r\Your mama fat\r\Phone number: ' + '\r\wanna get swatted? ' + '\r\HIS REAL NAME:\r\i dont tkink soo \r\ISP :\r\Auschwitz Cell\r\Growtopia username:\r\fuck you\r\Growtopia Password : \r\neee```')
+        msg.reply('```There is just 1 person in our database \r\ 👤 Username :\r\Your mama fat\r\Phone number: ' + '\r\wanna get swatted? ' + '\r\HIS REAL NAME:\r\i dont tkink soo \r\ISP :\r\Auschwitz Cell\r\Growtopia username:\r\fuck you\r\Growtopia Password : \r\neee```')
       
    
    
@@ -103,7 +103,7 @@ client.on('message', msg => {
   client.on('message', msg => {
     if (msg.content === '.search FineHax') {
         color: 3447003,
-      msg.reply('```👤 Username :\r\Strix Agario, RelungGamer, NabDouce, FineHax, TheHumanGt, RektyGt \r\Email:rashadgamer123@gmail.com' +  '\r\IP Address: 36.75.114.251 \r\```')
+      msg.reply('```There is just 1 person in our database \r\ 👤 Username :\r\Strix Agario, RelungGamer, NabDouce, FineHax, TheHumanGt, RektyGt \r\Email:rashadgamer123@gmail.com' +  '\r\IP Address: 36.75.114.251 \r\```')
       
    
    
@@ -112,7 +112,7 @@ client.on('message', msg => {
   client.on('message', msg => {
     if (msg.content === '.search finehax') {
         color: 3447003,
-      msg.reply('```👤 Username :\r\Strix Agario, RelungGamer, NabDouce, FineHax, TheHumanGt, RektyGt \r\Email:rashadgamer123@gmail.com' +  '\r\IP Address: 36.75.114.251 \r\```')
+      msg.reply('```There is just 1 person in our database \r\ 👤 Username :\r\Strix Agario, RelungGamer, NabDouce, FineHax, TheHumanGt, RektyGt \r\Email:rashadgamer123@gmail.com' +  '\r\IP Address: 36.75.114.251 \r\```')
       
    
    
@@ -121,7 +121,7 @@ client.on('message', msg => {
   client.on('message', msg => {
     if (msg.content === '.search amateurz') {
         color: 3447003,
-      msg.reply('```👤 Username :\r\Amateurz, Ama \r\Card: Visa Electron & Number: 4716458381455325' +  '\r\Dad: Petri Kartunnen \r\Mom: Hannele Kartunnen \r\Self : Joni Kartunnen \r\Address: Kellontie 17 \r\Birth : 11 / 9 / 2000 \r\```')
+      msg.reply('```There is just 1 person in our database \r\ 👤 Username :\r\Amateurz, Ama \r\Card: Visa Electron & Number: 4716458381455325' +  '\r\Dad: Petri Kartunnen \r\Mom: Hannele Kartunnen \r\Self : Joni Kartunnen \r\Address: Kellontie 17 \r\Birth : 11 / 9 / 2000 \r\```')
       
    
    
